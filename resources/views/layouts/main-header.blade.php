eb50b28223d2272ef47f42f02c9c85b2c3481e73
Eng-AkramShaker <akramaboubakersh@gmail.com>
2025-01-26 20:24:32 +0200

    26/1/2025

---
+++ resources/views/layouts/main-header.blade.php
@@ -0,0 +1,43 @@
+<!-- main-header opened -->
+
+<!-- Basic -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<!-- Mobile Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Site Metas -->
+
+<title>@yield('title') </title>
+
+
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Site Icons -->
+<link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css') }}">
+
+<!-- Site CSS -->
+<link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
+
+<!-- ALL VERSION CSS -->
+<link rel="stylesheet" href="{{ URL::asset('assets/css/versions.css') }}">
+
+<!-- Responsive CSS -->
+<link rel="stylesheet" href="{{ URL::asset('assets/css/responsive.css') }}">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="{{ URL::asset('assets/css/custom.css') }}">
+
+
+<!-- FontAwesome CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
+
+<!-- /main-header -->
